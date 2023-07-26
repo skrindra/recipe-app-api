aea9bef0e875d0bf31da65e0aeb2ad84ddc58f94
sivakumar <courses.skr@gmail.com>
2023-07-26 07:41:21 +0530

    added wait_for_db command

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,12 +3,11 @@ calculator functions
 """
 
 
-def add(x,y):
+def add(x, y):
     """Adds x and y and returns sum"""
     return x + y
 
-def subtract(x,y):
+
+def subtract(x, y):
     """Subtracts x from y and return difference"""
     return x - y
-
-
---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,28 @@
+"""
+Django command to wait for database to be available
+"""
+
+from django.core.management.base import BaseCommand
+
+from psycopg2 import OperationalError as psycopg2OpError
+from django.db.utils import OperationalError
+import time
+
+
+class Command(BaseCommand):
+    """Django command to wait for database"""
+
+    def handle(self, *args, **options):
+        """Entrypoint for command."""
+        self.stdout.write('Waiting for database')
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (psycopg2OpError, OperationalError):
+                self.stdout.write('Database unavailable, waiting \
+                                  for 1 second...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database Available!'))
